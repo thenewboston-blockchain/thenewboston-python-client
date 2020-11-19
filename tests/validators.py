38405513fa8bf83f79c305c681b50911714908e7
Velibor <veliborvasiljevic7@gmail.com>
2020-11-19 20:22:20 +0100

    validators fetch accounts tests fixed

--- tests/validators.py
+++ tests/validators.py
@@ -2,14 +2,28 @@ from tnb.validators import Validator
 
 
 def test_success_fetch_accounts(requests_mock):
-    result = [
+
+    accounts = [
         {
             "id": "4cb1cdbe-ebbf-43c8-9f86-826aaa2af250",
             "account_number": "9bfa37627e2dba0ae48165b219e76ceaba036b3db8e84108af73a1cce01fad35",
             "balance": 6,
             "balance_lock": "749f6faa4eeeda50f51334e903a1eaae084435d53d2a85fb0993a518fef27273",
-        }
+        },
+        {
+            "id": "9c6dd61a-438c-4a95-b1d2-33f90bd7f6ad",
+            "account_number": "2e86f48216567302527b69eae6c6a188097ed3a9741f43cc3723e570cf47644c",
+            "balance": 380,
+            "balance_lock": "aca94f4d2f472c6b9b662f60aab247b9c6aef2079d63b870e2cc02308a7c822b",
+        },
     ]
+
+    result = {
+        "count": 2,
+        "next": None,
+        "previous": None,
+        "results": accounts,
+    }
 
     requests_mock.get("http://42.0.6.9:80/accounts", json=result)
 
